592134122a27d45d80a49d2f97612b39b6ecfb8c
astralsandwich6324 <santillangarciaj659@gmail.com>
2024-10-26 22:46:45 -0400

    Tarea 2
    Tarea2

---
+++ src/Tarea2Solution/Tarea2/Data/Tarea2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tarea2.Models.Entities;
+
+namespace Tarea2.Data
+{
+    public class Tarea2Context : DbContext
+    {
+        public Tarea2Context (DbContextOptions<Tarea2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tarea2.Models.Entities.Customer> Customers { get; set; }
+    }
+}
