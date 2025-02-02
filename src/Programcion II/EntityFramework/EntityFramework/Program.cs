462189d8e4e7974a198e7a1589889c1434da1722
astralsandwich6324 <santillangarciaj659@gmail.com>
2025-02-02 16:06:30 -0400

    creating carpet

---
+++ src/Programcion II/EntityFramework/EntityFramework/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text;
+
+byte[] data = Encoding.UTF8.GetBytes("This is my text");
+
+var text = Encoding.UTF8.GetString(data);
+
+Console.WriteLine(text);
---
+++ src/Programcion II/Learnign Algorithms/Learnign Algorithms/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//fizzbuzz algorithms
+/*foreach (int num in number)
+{
+    if (num % 15 == 0)
+    {
+        Console.WriteLine($"{num} Fizzbuzz");
+    }
+    else if (num % 5 == 0)
+    {
+        Console.WriteLine($"{num} buzz");
+    }
+    else if(num % 3 == 0 )
+    {
+        Console.WriteLine($"{num} fizz");
+
+    }
+    else
+    {
+        Console.WriteLine(num);
+    }
+}
+*/
+
+
